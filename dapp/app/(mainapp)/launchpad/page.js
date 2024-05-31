68ea21ae874ff2c0dff2e4c7dc3dc441aa5670e8
Ninja Messi <ton33157@gmail.com>
2024-05-31 10:38:49 -0700

    fix some issue

--- dapp/app/(mainapp)/launchpad/page.js
+++ dapp/app/(mainapp)/launchpad/page.js
@@ -1,101 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
-
-// import Image from "next/image";
-// import React, { useEffect, useState } from "react";
-// import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-
-// import axios from "axios";
-// import UserContext from "@/lib/context";
-// import Link from "next/link";
-// import LoadingComponent from "@/components/Loading";
-
-// export default function LanuchpadPage() {
-//   const [totalCollections, setTotalCollections] = useState<any[]>([]);
-//   const [myCollections, setMyCollections] = useState<any[]>([]);
-//   const [collections, setCollections] = useState<any[]>([]);
-
-//   const { accountId } = React.useContext(UserContext);
-
-//   useEffect(() => {
-//     void axios
-//       .post(
-//         "https://api.thegraph.com/subgraphs/name/icetrust0212/defishards-test",
-//         {
-//           query: `
-//             query collection {
-//               collections {
-//                 id
-//                 name
-//                 symbol
-//                 price
-//                 base_uri
-//                 currency
-//                 payment_split_percent
-//                 totalSupply
-//                 creator {id}
-//               }
-//             }
-//           `,
-//         },
-//       )
-//       .then((res) => {
-//         const totalCollections = res.data.data.collections;
-//         setTotalCollections(totalCollections);
-//         const myCollections = totalCollections.filter((collection: any) => {
-//           return collection.creator.id === accountId;
-//         });
-//         setMyCollections(myCollections);
-//         setCollections(totalCollections);
-//       });
-//   }, [accountId]);
-
-//   if (totalCollections.length === 0) {
-//     return <LoadingComponent />;
-//   }
-
-//   return (
-//     <div className="container mx-auto my-16 max-sm:px-4">
-//       <h2 className="flex items-center justify-between text-4xl max-sm:flex-col gap-4">
-//         Total Collections
-//         <Link
-//           href={"/create"}
-//           className="effect-shadow-button inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md bg-gradient-to-r from-[#8E4CE2] to-[#E19882] px-4 py-2 text-sm font-medium text-button-text ring-offset-background transition-colors hover:bg-accent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-//         >
-//           Create a new collection
-//         </Link>
-//       </h2>
-//       <div className="mt-10 grid grid-cols-4 gap-6 max-lg:grid-cols-2 max-sm:grid-cols-1">
-//         {totalCollections.map((item, _index) => (
-//           <Link
-//             href={`/launchpad/${item.id}`}
-//             className="w-full cursor-pointer rounded-lg bg-[#F2F2F2] p-4 dark:bg-[#131315B2]"
-//             key={_index}
-//           >
-//             <AspectRatio ratio={16 / 12}>
-//               <Image
-//                 src={`${item.base_uri}0.png`}
-//                 alt="collection"
-//                 width={400}
-//                 height={300}
-//                 className="h-full w-full rounded-lg object-cover"
-//               />
-//             </AspectRatio>
-//             <span className="mt-4 block text-xs">@{item.creator.id}</span>
-//             <p className="my-2">
-//               {item.name} ({item.symbol})
-//             </p>
-//             <p className="mb-5 mt-5 flex justify-between text-sm">
-//               My holdings <span>2</span>
-//             </p>
-//           </Link>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
 
 import Button from "../../components/Button";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
@@ -112,7 +15,7 @@ import {
 import LoadingComponent from "../../components/Loading";
 
 export default function LanuchpadPage() {
-  const collection_id = "defis.test-defishard-launchpad.testnet";
+  const collection_id = "defin.test-defishard-launchpad.testnet";
   const [collection, setCollectionData] = useState({});
   const { accountId, walletSelector, signInModal, walletSelectorObject } =
     useContext(UserContext);
